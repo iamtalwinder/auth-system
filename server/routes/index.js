6f50469b851f0936c50e832404b269d121cf9ab8
Talwinder Singh <singhtalwinder790@gmail.com>
2020-09-30 22:29:42 +0530

    Added session based signin

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const con = require("../config/db");
 const userController = require("../controller/user");
 
 const router = express.Router();
 
 router.post("/signup", userController.signup);
+router.post("/signin", userController.signin);
+router.get(
+  "/test",
+  (req, res, next) => {
+    if (!req.session.user) {
+      return res.status(401).send({ msg: "Unauthorized" });
+    }
+    next();
+  },
+  (req, res) => {
+    res.status(200).send("Protected route");
+  }
+);
 
 module.exports = router;
