95ca8ec43ef3472b6e6f2d188c00aa722d7bfe34
Talwinder Singh <singhtalwinder790@gmail.com>
2020-10-03 16:08:07 +0530

    Setup

---
+++ client/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Auth System</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="text-purple-500">Hello world</div>
+    </>
+  );
+}
